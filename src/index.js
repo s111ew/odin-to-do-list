427ef1d83e208e9812970ee250eca3f0460eb7bb
s111ew <167631312+s111ew@users.noreply.github.com>
2024-08-21 16:38:04 +0100

    Add logic to dynamically render projects section

--- src/index.js
+++ src/index.js
@@ -1,2 +1,52 @@
 import "./style.css"
 
+import projects from './projects.json' assert { type: 'json' };
+
+import editIconSvg from './images/edit.svg'
+
+function paintProjects() {
+    projects.forEach((project) => {
+        let numOfUrgentToDos = 0;
+        project["toDo"].forEach(toDo => {
+            if (toDo.priority === 3) {
+                numOfUrgentToDos++
+            }
+        })
+        createProjectElement(project.projectName, project["toDo"].length, numOfUrgentToDos, project["toDo"][0]["due"]);
+    })
+}
+
+function createProjectElement(projName, numOfToDos, numOfUrgents, nextDueDate) {
+    const projectsGrid = document.querySelector(".projects-grid");
+
+    const projectContainer = document.createElement("div");
+    projectContainer.classList.add("project");
+
+    const projectHeader = document.createElement("div");
+    projectHeader.classList.add("project-header");
+
+    const projectName = document.createElement("h3");
+    projectName.textContent = projName;
+
+    const editIcon = document.createElement("img");
+    editIcon.alt = "edit icon";
+    editIcon.src = editIconSvg;
+    
+    projectHeader.appendChild(projectName);
+    projectHeader.appendChild(editIcon);
+
+    projectContainer.appendChild(projectHeader);
+
+    const projectContains = document.createElement("span");
+    projectContains.textContent = `Contains ${numOfToDos} to-dos (${numOfUrgents} Urgent)`;
+
+    const projectDueDate = document.createElement("span");
+    projectDueDate.textContent = `Next due date ${nextDueDate}`;
+
+    projectContainer.appendChild(projectContains);
+    projectContainer.appendChild(projectDueDate);
+
+    projectsGrid.appendChild(projectContainer);
+}
+
+window.onload = paintProjects();
